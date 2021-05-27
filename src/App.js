98d2958cd1c119642f156017c6b831561b563322
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-05-27 07:41:30 -0700

    removed app; import weather
    removed app from index

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <h1>
           Destination Weather
         </h1>
-      </header>
     </div>
   );
 }
